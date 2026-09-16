f474fe781e38c2ec2d239bef6c7f22097c3c70c6
73158851 <angelitoortiz101@gmail.com>
2026-09-16 07:23:03 -0400

    Usar una sola galeria y cargar mejoras del panel

--- router.js
+++ router.js
@@ -2,8 +2,8 @@ const admin=location.pathname.startsWith('/admin');
 if(admin){
   await import('/app.js');
   await import('/multi-images.js');
-  await import('/image-manager-v2.js');
   await import('/admin-brand.js');
+  await import('/admin-fixes.js');
 }else{
   await import('/storefront.js');
 }
