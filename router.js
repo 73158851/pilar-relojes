f5e5045e3fb40b8b691168133eefe937800ac723
73158851 <angelitoortiz101@gmail.com>
2026-09-16 23:03:45 -0400

    Actualizar activacion PWA de administrador

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ if(admin){
   await import('/admin-v2.js?v=20260916-admin-core-2');
   await import('/admin-enhance.js?v=20260916-save-fix-1');
   await import('/admin-sales.js?v=20260916-sales-2');
-  await import('/admin-pwa.js?v=20260917-admin-pwa-1');
+  await import('/admin-pwa.js?v=20260917-admin-pwa-2');
 }else{
   await import('/storefront.js');
   await import('/store-enhance.js');
